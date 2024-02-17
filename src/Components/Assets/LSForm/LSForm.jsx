ee56677d49e73caad1a7a1edf333da90e7543c49
FriderinTuk <fridadenhitf@gmail.com>
2024-02-16 21:02:15 -0600

    esqueleto del formulario listo, falta embellecerlo

--- src/Components/Assets/LSForm/LSForm.jsx
+++ src/Components/Assets/LSForm/LSForm.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import "./LSForm.css"
 
 import user_icon from "../usuario.png"
@@ -6,34 +6,39 @@ import mail_icon from "../mail.png"
 import password_icon from "../password.png"
 
 const LSForm = () => {
+
+    const [action,setAction]= useState("Registro");
+
     return(
         <div className="container">
             <div className="header">
-            <div className="text">Registrarse</div>
+            <div className="text"> {action} </div>
             <div className="underline"></div>
             </div>
             <div className="inputs">
+            {action==="Registro"?<div></div>: <div className="input">
+                <img src= {user_icon} className="icon" alt="ícono de usuarie" />
+                <input type="text" placeholder="Nombre" />
+            </div> }
+           
+            </div>
+            <div className="inputs">
             <div className="input">
-                <img src= {user_icon} alt="" />
-                <input type="text"/>
+                <img src= {mail_icon} className="icon" alt="ícono de email" />
+                <input type="email" placeholder="Correo" />
             </div>
             </div>
             <div className="inputs">
             <div className="input">
-                <img src= {mail_icon} alt="" />
-                <input type="email"/>
+                <img src= {password_icon} className="icon" alt="ícono de contraseña" />
+                <input type="password" placeholder="Contraseña" />
             </div>
             </div>
-            <div className="inputs">
-            <div className="input">
-                <img src= {password_icon} alt="" />
-                <input type="password"/>
-            </div>
-            </div>
-            <div className="forgot-password">¿Olvidaste tu contraseña? <span>Click aquí</span> </div>
+            {action==="Registro"?<div></div>:<div className="forgot-password">¿Olvidaste tu contraseña? <span>Click aquí</span> </div> }
+            
             <div className="submit-container">
-                <div className="submit">Registrarse</div>
-                <div className="submit">Entrar</div>
+                <div className= {action==="Entrar"?"submit gray":"submit"} onClick={()=>{setAction("Registro")}} >Registro</div>
+                <div className= {action==="Registro"?"submit gray":"submit"} onClick={()=>{setAction("Entrar")}} >Entrar</div>
             </div>
         </div>
     )

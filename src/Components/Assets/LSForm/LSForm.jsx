b5285a8e9d85b36fcf2fec25b6f438ad17855c31
FriderinTuk <fridadenhitf@gmail.com>
2024-02-23 19:55:58 -0600

    Mejoré la legibilidad del código (indentación y el uso de valores booleanos) y subrayé el "click aquí"

--- src/Components/Assets/LSForm/LSForm.jsx
+++ src/Components/Assets/LSForm/LSForm.jsx
@@ -8,37 +8,58 @@ import password_icon from "../password.png"
 const LSFormulario = () => {
 
     const [action,setAction]= useState("Registro");
+    const [showName, setShowName] = useState(true);//esta de aquí es para usar valores booleanos en el código y que esté más limpio y sea más sencillo de leer
 
     return(
         <div className="container">
             <div className="header">
-            <div className="text"> {action} </div>
-            <div className="underline"></div>
+                <div className="text"> {action} </div>
+                <div className="underline"></div>
             </div>
             <div className="inputs">
-            {action==="Ingresar"?<div></div>: <div className="input">
-                <img src= {user_icon} alt="ícono de usuarie" />
-                <input id="Name" type="text" required placeholder="Nombre" />
-            </div> }
+                {showName && (
+                    <div className="input">
+                        <img src= {user_icon} alt="ícono de usuarie" />
+                        <input id="Name" type="text" required placeholder="Nombre" />
+                </div> )}
            
             </div>
             <div className="inputs">
-            <div className="input">
-                <img src= {mail_icon} alt="ícono de email" />
-                <input id="email" type="email" required placeholder="Correo" />
-            </div>
+                <div className="input">
+                    <img src= {mail_icon} alt="ícono de email" />
+                    <input id="email" type="email" required placeholder="Correo" />
+                </div>
             </div>
             <div className="inputs">
-            <div className="input">
-                <img src= {password_icon} alt="ícono de contraseña" />
-                <input id="password" type="password" minLength="8" pattern="[a-z0-5]{8}" required placeholder="Contraseña" />
+                <div className="input">
+                    <img src= {password_icon} alt="ícono de candado" />
+                    <input id="password" type="password" minLength="8" pattern="[a-z0-5]{8}" required placeholder="Contraseña" />
+                </div>
             </div>
-            </div>
-            {action==="Registro"?<div></div>:<div className="forgot-password">¿Olvidaste tu contraseña? <span>Click aquí</span> </div> }
+            {action==="Registro" &&(    
+                <div  className="forgot-password">¿Olvidaste tu contraseña? <span>Click aquí</span> </div> 
+            )}
             
             <div className="submit-container">
-                <div className= {action==="Ingresar"?"submit gray":"submit"} onClick={()=>{setAction("Registro")}} > Registro</div>
-                <div className= {action==="Registro"?"submit gray":"submit"} onClick={()=>{setAction("Ingresar")}} >Ingresar</div>
+                <div
+                    className={action === "Ingresar"? "submit gray" : "submit"}
+                    onClick={()=> {
+                        setAction("Registro");
+                        setShowName(true); //esto muestra el nombre cuando le das en "regsitro" 
+                    }} //el {" "} es sólo un espacio en blanco que se deja por fines de mejorar la legibilidad 
+                > 
+                    {" "} 
+                    Registro
+                </div> 
+                <div 
+                    className={action==="Registro"?"submit gray":"submit"}
+                    onClick={()=>{
+                        setAction("Ingresar");
+                        setShowName(false); //esto oculta el campo de nombre cuando le dan en ingresar
+                    }}
+                >
+                    Ingresar
+                </div>
             </div>
         </div>
     )

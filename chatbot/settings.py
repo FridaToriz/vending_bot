5ec6cb67e916b5268c35e3fc3fe485e4e8eace0a
Diana Escatel <dianaescatel33@gmail.com>
2024-03-29 17:44:04 -0600

    configuracion de Database

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chatbotapp',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatbot',
+        'USER': 'diana',
+        'PASSWORD': 'psql333',  # Empty, it has no pw
+        'HOST': 'localhost',  # 127.0.0.1
+        'PORT': '5432',  
     }
 }
+
 
 
 # Password validation
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -16,7 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import (
+    UsersListCreate,
+    UsersRetrieveUpdateDestroy,
+    ProductListCreate,
+    ProductRetrieveUpdateDestroy,
+    ProfileListCreate,
+    ProfileRetrieveUpdateDestroy,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+# Routes for users
+    path('users/', UsersListCreate.as_view(), name='user-list'),
+    path('users/<int:pk>/', UsersRetrieveUpdateDestroy.as_view(), name='user-detail'),
+
+# Routes for products
+    path('products/', ProductListCreate.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroy.as_view(), name='product-detail'),
+
+# Routes for profiles
+    path('profiles/', ProfileListCreate.as_view(), name='profile-list'),
+    path('profiles/<int:pk>/', ProfileRetrieveUpdateDestroy.as_view(), name='profile-detail'),
 ]
